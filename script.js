773e5b7b3c82b706d55639533c801248682f540a
Gabriel Cagliari Luz <gabrielluz.git.hub@gmail.com>
2026-06-16 21:33:43 -0300

    Adiciona ouvinte de evento nos campos de input

--- script.js
+++ script.js
@@ -39,3 +39,67 @@ form.addEventListener('submit', (event) =>{
         successBox.style.display = 'none'
     }, 3000)
 })
+
+const nameInput = document.getElementById('name')
+
+nameInput.addEventListener('input', () =>{
+    if (nameInput.value.trim() !== '') {
+        nameInput.classList.remove('erro')
+        nameInput.classList.add('sucesso')
+    } else {
+        nameInput.classList.remove('sucesso')
+        nameInput.classList.add('erro')
+    }
+})
+
+const videoInput = document.getElementById('video')
+const videoContainer = videoInput.parentElement;
+
+videoInput.addEventListener('input', () =>{
+    const videoValue = videoInput.value.trim();
+    if (videoValue === '' || (!videoValue.includes('youtube.com') && !videoValue.includes('youtu.be'))) {
+        videoContainer.classList.remove('sucesso');
+        videoContainer.classList.add('erro');
+    } else {
+        videoContainer.classList.remove('erro');
+        videoContainer.classList.add('sucesso');
+    }
+})
+
+const birthdateInput = document.getElementById('birthdate');
+
+birthdateInput.addEventListener('change', () =>{
+    if (birthdateInput.value !== '') {
+        birthdateInput.classList.remove('erro');
+        birthdateInput.classList.add('sucesso');
+    } else {
+        birthdateInput.classList.remove('sucesso');
+        birthdateInput.classList.add('erro');
+    }
+})
+
+const positionInput = document.getElementById('position');
+
+positionInput.addEventListener('change', () =>{
+    if(positionInput.value !== '') {
+        positionInput.classList.remove('erro');
+        positionInput.classList.add('sucesso');
+    } else {
+        positionInput.classList.remove('sucesso');
+        positionInput.classList.add('erro');
+    }
+})
+
+const instagramInput = document.getElementById('instagram');
+const instagramContainer = instagramInput.parentElement;
+
+instagramInput.addEventListener('input', () =>{
+    if (instagramInput.value.trim() !== '') {
+        instagramContainer.classList.remove('erro')
+        instagramContainer.classList.add('sucesso')
+    } else {
+        instagramContainer.classList.remove('sucesso')
+        instagramContainer.classList.add('erro')
+
+    }
+})
